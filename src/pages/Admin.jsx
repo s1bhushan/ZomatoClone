6f1338cf2ebb1c1337367c9b7124f872511145a2
Bhushan Sonawane <bsonawane990@gmail.com>
2025-05-27 12:56:25 +0530

    admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -6,7 +6,10 @@ export default function Admin() {
     <div>
 
          <div className="pt-40">
-               <h1>This is admin Page</h1>
+               <li className="p-4 border rounded-md shadow">
+              <h3 className="text-lg font-semibold">Admin page</h3>
+              <p className="text-slate-600">This is an Admin page</p>
+               </li>
              </div>
 
 <Footer/>
